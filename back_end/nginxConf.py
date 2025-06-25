ae3a8f970883537df312be4dbc8565490867e0ac
Baastheglass <lightningblazer123@gmail.com>
2025-06-25 23:34:43 +0500

    configuration string added

--- back_end/nginxConf.py
+++ back_end/nginxConf.py
@@ -10,6 +10,21 @@ def configureSitesAvailable(subdomain, port):
     while(current_dir != '/'):
         os.chdir("..")
         current_dir = os.getcwd()
+    server_configuration_string = f"""
+        server {{
+            listen 80;
+            listen [::]:80;
+            server_name {subdomain};
+            
+            location / {{
+                proxy_pass http://127.0.0.1:{port};
+                proxy_set_header Host $host;
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_set_header X-Forwarded-Proto $scheme;
+            }}
+        }}
+    """
     with open("etc/nginx/sites-available/default", "a") as f:
         f.write("This is my appended string.\n")
 def configureSitesEnabled(subdomain, port):
@@ -17,6 +32,21 @@ def configureSitesEnabled(subdomain, port):
     while(current_dir != '/'):
         os.chdir("..")
         current_dir = os.getcwd()
+    server_configuration_string = f"""
+        server {{
+            listen 80;
+            listen [::]:80;
+            server_name {subdomain};
+            
+            location / {{
+                proxy_pass http://127.0.0.1:{port};
+                proxy_set_header Host $host;
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_set_header X-Forwarded-Proto $scheme;
+            }}
+        }}
+    """
     with open("etc/nginx/sites-enabled/default", "a") as f:
         f.write("This is my appended string.\n")        
 
