e87532d78d7155bd4238524e9fcdc0315b41f1dd
Muhammad Baasil <122256730+Baastheglass@users.noreply.github.com>
2025-07-10 19:02:31 +0500

    Update nginxConf.py

--- back_end/nginxConf.py
+++ back_end/nginxConf.py
@@ -10,19 +10,33 @@ def configureSitesAvailable(subdomain, port):
         os.chdir("..")
         current_dir = os.getcwd()
     server_configuration_string = f"""
-    server {{
-        listen 80;
-        listen [::]:80;
-        server_name {subdomain};
-        
-        location / {{
-            proxy_pass http://127.0.0.1:{port};
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-        }}
-    }}"""
+    server {
+    listen 443 ssl;
+    listen [::]:443 ssl;
+    server_name {subdomain};
+
+    ssl_certificate /etc/letsencrypt/live/axonbuild.com/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/axonbuild.com/privkey.pem;
+
+    ssl_protocols TLSv1.2 TLSv1.3;
+    ssl_ciphers HIGH:!aNULL:!MD5;
+
+    location / {
+        proxy_pass http://127.0.0.1:{port};
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+}
+server {
+    listen 80;
+    listen [::]:80;
+    server_name {subdomain};
+
+    return 301 https://$host$request_uri;
+}
+        """
     with open("etc/nginx/sites-available/default", "a") as f:
         f.write(server_configuration_string)
 
